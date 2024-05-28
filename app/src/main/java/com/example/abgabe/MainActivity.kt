ce1b29dd56f6c84e8f940e76e6fd4fc8cd682c04
Lumoma <126558513+Lumoma@users.noreply.github.com>
2024-05-28 18:48:17 +0200

    Saved the Working AR Base

--- app/src/main/java/com/example/abgabe/MainActivity.kt
+++ app/src/main/java/com/example/abgabe/MainActivity.kt
@@ -1,28 +1,52 @@
 package com.example.abgabe
 
+import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.GLSurfaceView.Renderer
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.abgabe.ar.common.helpers.DisplayRotationHelper
+import com.example.abgabe.ar.common.helpers.SnackbarHelper
+import com.example.abgabe.ar.common.helpers.TrackingStateHelper
+import com.example.abgabe.ar.common.rendering.AugmentedImageRenderer
+import com.example.abgabe.ar.common.rendering.BackgroundRenderer
 import com.example.abgabe.ui.theme.AbgabeTheme
+import com.google.ar.core.Anchor
+import com.google.ar.core.AugmentedImage
+import com.google.ar.core.Frame
+import com.google.ar.core.Session
+import com.google.ar.core.TrackingState
+import java.io.IOException
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+
+private lateinit var displayRotationHelper: DisplayRotationHelper
+private lateinit var backgroundRenderer: BackgroundRenderer
+private lateinit var augmentedImageRenderer: AugmentedImageRenderer
+private var trackingStateHelper = TrackingStateHelper(null)
+private val messageSnackbarHelper = SnackbarHelper()
+private lateinit var session: Session
+private val augmentedImageMap: MutableMap<Int, Pair<AugmentedImage, Anchor>> = mutableMapOf()
+
 
 class MainActivity : ComponentActivity(), Renderer {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        displayRotationHelper = DisplayRotationHelper(this)
+        backgroundRenderer = BackgroundRenderer()
+        augmentedImageRenderer = AugmentedImageRenderer()
+        session = Session(this)
+        trackingStateHelper = TrackingStateHelper(this)
+
+        //setContentView(R.layout.activity_main)
+        //TODO: val fitToScanView: ImageView = findViewById(R.id.fit_to_scan_view)
         enableEdgeToEdge()
         setContent {
             AbgabeTheme {
@@ -46,20 +70,126 @@ class MainActivity : ComponentActivity(), Renderer {
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        // Set the background color to black
-        gl?.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f)
+
+
+        // Prepare the rendering objects. This involves reading shaders, so may throw an IOException.
+        try {
+            // Create the texture and pass it to ARCore session to be filled during update().
+            backgroundRenderer.createOnGlThread(this)
+            augmentedImageRenderer.createOnGlThread(this)
+        } catch (e: IOException) {
+            Log.e(
+                "Abgabe",
+                "Failed to read an asset file",
+                e
+            )
+        }
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-        // Set the viewport to the entire surface
-        gl?.glViewport(0, 0, width, height)
+        displayRotationHelper.onSurfaceChanged(width, height)
+        GLES20.glViewport(0, 0, width, height)
     }
 
     override fun onDrawFrame(gl: GL10?) {
-        // Clear the surface
-        gl?.glClear(GL10.GL_COLOR_BUFFER_BIT)
+        // Clear screen to notify driver it should not load any pixels from previous frame.
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
+
+        if (session == null) {
+            return
+        }
+
+
+        // Notify ARCore session that the view size changed so that the perspective matrix and
+        // the video background can be properly adjusted.
+        displayRotationHelper.updateSessionIfNeeded(session)
+
+        try {
+            session.setCameraTextureName(backgroundRenderer.textureId)
+
+            // Obtain the current frame from ARSession. When the configuration is set to
+            // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
+            // camera framerate.
+            val frame: Frame = session.update()
+            val camera = frame.camera
+
+            // Keep the screen unlocked while tracking, but allow it to lock when tracking stops.
+            trackingStateHelper.updateKeepScreenOnFlag(camera.trackingState)
+
+            // If frame is ready, render camera preview image to the GL surface.
+            backgroundRenderer.draw(frame)
+
+            // Get projection matrix.
+            val projmtx = FloatArray(16)
+            camera.getProjectionMatrix(projmtx, 0, 0.1f, 100.0f)
+
+            // Get camera matrix and draw.
+            val viewmtx = FloatArray(16)
+            camera.getViewMatrix(viewmtx, 0)
+
+            // Compute lighting from average intensity of the image.
+            val colorCorrectionRgba = FloatArray(4)
+            frame.lightEstimate.getColorCorrection(colorCorrectionRgba, 0)
+
+            // Visualize augmented images.
+            drawAugmentedImages(frame, projmtx, viewmtx, colorCorrectionRgba)
+        } catch (t: Throwable) {
+            // Avoid crashing the application due to unhandled exceptions.
+            Log.e(
+                "Abgabe",
+                "Exception on the OpenGL thread",
+                t
+            )
+        }
+    }
+
+    private fun drawAugmentedImages(
+        frame: Frame,
+        projmtx: FloatArray,
+        viewmtx: FloatArray,
+        colorCorrectionRgba: FloatArray
+    ) {
+        val updatedAugmentedImages = frame.getUpdatedTrackables(AugmentedImage::class.java)
+
+        // Iterate to update augmentedImageMap, remove elements we cannot draw.
+        for (augmentedImage in updatedAugmentedImages) {
+            when (augmentedImage.trackingState) {
+                TrackingState.PAUSED -> {
+                    // When an image is in PAUSED state, but the camera is not PAUSED, it has been detected,
+                    // but not yet tracked.
+                    val text = String.format("Detected Image %d", augmentedImage.index)
+                    messageSnackbarHelper.showMessage(this, text)
+                }
+
+                TrackingState.TRACKING -> {
+                    //TODO
+                    // Have to switch to UI Thread to update View.
+                    /*
+                    runOnUiThread {
+                        fitToScanView.visibility = View.GONE
+                    }
+                    */
+
+                    // Create a new anchor for newly found images.
+                    if (!augmentedImageMap.containsKey(augmentedImage.index)) {
+                        val centerPoseAnchor = augmentedImage.createAnchor(augmentedImage.centerPose)
+                        augmentedImageMap[augmentedImage.index] = Pair(augmentedImage, centerPoseAnchor)
+                    }
+                }
+
+                TrackingState.STOPPED -> {
+                    augmentedImageMap.remove(augmentedImage.index)
+                }
+
+                else -> {
+                }
+            }
+        }
     }
 }
+
+
 
 
 /*
