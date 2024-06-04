76d105b17817d2b1b6e4a030f7f1c64b8d5e3bf1
Lumoma <126558513+Lumoma@users.noreply.github.com>
2024-06-04 17:47:42 +0200

    Settings Viewmodel MVVM now working

--- app/src/main/java/com/example/abgabe/ui/views/SettingsUI.kt
+++ app/src/main/java/com/example/abgabe/ui/views/SettingsUI.kt
@@ -1,6 +1,5 @@
 package com.example.abgabe.ui.views
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -53,23 +52,21 @@ object SettingsUI {
 
             Column(modifier = modifier.verticalScroll(rememberScrollState())) {
                 when (uiState) {
-                    SettingsUiState.Loading -> LoadingState(text = "Loading database...")
-                    SettingsUiState.DumpingDatabase -> LoadingState(text = "Dumping database...")
-                    is SettingsUiState.Content -> ContentState(
-                        uiState.amount,
+                    is SettingsUiState.Loading -> LoadingState(text = "Loading database...")
+
+                    is SettingsUiState.DumpingDatabase -> LoadingState(text = "Dumping database...")
+
+                    is SettingsUiState.Content -> ContentState(uiState.amount,
                         onNavigateToOverview,
-                        viewModel::onDumpDatabaseClicked,
-                        { amount -> (viewModel::onGenerateCatsClicked)(amount) }
-                    )
-                    SettingsUiState.DatabaseEmpty -> {
-                        // Hier kannst du entweder den Dialog anzeigen oder eine andere UI für eine leere Datenbank
-                        ShowDialog({ amount -> (viewModel::onGenerateCatsClicked)(amount) })
-                        ContentState(
-                            0,
+                        viewModel::onDumpDatabaseClicked
+                    ) { amount -> (viewModel::onGenerateCatsClicked)(amount) }
+
+                     is SettingsUiState.DatabaseEmpty -> {
+                        ShowDialog { amount -> (viewModel::onGenerateCatsClicked)(amount) }
+                        ContentState(0,
                             onNavigateToOverview,
-                            viewModel::onDumpDatabaseClicked,
-                            { amount -> (viewModel::onGenerateCatsClicked)(amount) }
-                        )
+                            viewModel::onDumpDatabaseClicked
+                        ) { amount -> (viewModel::onGenerateCatsClicked)(amount) }
                     }
                 }
             }
@@ -77,49 +74,48 @@ object SettingsUI {
     }
 
     @Composable
-fun ContentState(
-    currentCatAmount: Int,
-    onNavigateToSettings: () -> Unit,
-    onDumpDatabase: () -> Unit,
-    onGenerateClick: (int: Int) -> Unit,
-) {
-    var catAmount by remember { mutableStateOf("") }
-    val keyboardController = LocalSoftwareKeyboardController.current
+    fun ContentState(
+        currentCatAmount: Int,
+        onNavigateToSettings: () -> Unit,
+        onDumpDatabase: () -> Unit,
+        onGenerateClick: (int: Int) -> Unit,
+    ) {
+        var catAmount by remember { mutableStateOf("") }
+        val keyboardController = LocalSoftwareKeyboardController.current
 
-    Button(onClick = { onNavigateToSettings() }) {
-        Text("Back to Overview")
-    }
-    Button(onClick = { onDumpDatabase() }) {
-        Text("Dump Database")
-    }
-
-    Text(text = "Cats in Database: $currentCatAmount")
-
-    if ((catAmount.toIntOrNull() ?: 0) > 0) {
-        Button(onClick = { onGenerateClick(catAmount.toIntOrNull() ?: 1) }) {
-            Text("Generate ${catAmount.toIntOrNull() ?: 1} new cats")
+        Button(onClick = { onNavigateToSettings() }) {
+            Text("Back to Overview")
         }
-    }
-    else {
-        Button(onClick = { onGenerateClick(10) }) {
-            Text("Generate 10 Cats")
+        Button(onClick = { onDumpDatabase() }) {
+            Text("Dump Database")
         }
-    }
 
-    TextField(
-        value = catAmount,
-        onValueChange = { catAmount = it },
-        label = { Text("Amount of new cats") },
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        keyboardActions = KeyboardActions(onSend = { keyboardController?.hide() })
-    )
-}
+        Text(text = "Cats in Database: $currentCatAmount")
+
+        if ((catAmount.toIntOrNull() ?: 0) > 0) {
+            val amount = catAmount.toIntOrNull() ?: 1
+            Button(onClick = { onGenerateClick(amount) }) {
+                Text(if (amount == 1) "Generate one more cat" else "Generate $amount more new cats")
+            }
+        } else {
+            Button(onClick = { onGenerateClick(10) }) {
+                Text("Generate 10 Cats")
+            }
+        }
+
+        TextField(value = catAmount,
+            onValueChange = { catAmount = it },
+            label = { Text("Amount of new cats") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            keyboardActions = KeyboardActions(onSend = { keyboardController?.hide() })
+        )
+    }
 
     @Composable
     fun LoadingState(
-        text: String,
-        modifier: Modifier = Modifier) {
+        text: String, modifier: Modifier = Modifier
+    ) {
 
         Box(modifier = Modifier.fillMaxSize()) {
             CircularProgressIndicator(modifier = modifier.align(Alignment.Center))
@@ -134,36 +130,26 @@ fun ContentState(
         var showDialog by remember { mutableStateOf(true) }
 
         if (showDialog) {
-            AlertDialog(
-                onDismissRequest = {
+            AlertDialog(onDismissRequest = {
+                showDialog = false
+            }, title = {
+                Text(text = "Database is empty!")
+            }, text = {
+                Text("Do you want to generate a new Database?")
+            }, confirmButton = {
+                Button(onClick = {
                     showDialog = false
-                },
-                title = {
-                    Text(text = "Database is empty!")
-                },
-                text = {
-                    Text("Do you want to generate a new Database?")
-                },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            showDialog = false
-                        }
-                    ) {
-                        Text("No")
-                    }
-                },
-                dismissButton = {
-                    Button(
-                        onClick = {
-                            showDialog = false
-                            onGenerateClick(10)
-                        }
-                    ) {
-                        Text("Yes, Generate new Database with 10 Cats")
-                    }
+                }) {
+                    Text("No")
                 }
-            )
+            }, dismissButton = {
+                Button(onClick = {
+                    showDialog = false
+                    onGenerateClick(10)
+                }) {
+                    Text("Yes, Generate new Database with 10 Cats")
+                }
+            })
         }
     }
 }
